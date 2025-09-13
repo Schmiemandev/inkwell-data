c7a6cae8aa3c67b8f09eb871036141ad603b4325
SSchmiemUU <144321355+SSchmiemUU@users.noreply.github.com>
2025-09-13 13:23:51 +0900

    Added validate_json.py to validate the json before pushing.

---
+++ validate_json.py
@@ -0,0 +1,18 @@
+
+import json
+import sys
+
+def validate_json(file_path):
+    try:
+        with open(file_path, 'r') as f:
+            json.load(f)
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON in {file_path}: {e}")
+        sys.exit(1)
+    except FileNotFoundError:
+        print(f"Error: {file_path} not found.")
+        sys.exit(1)
+    print(f"{file_path} is a valid JSON file.")
+
+if __name__ == "__main__":
+    validate_json('inks.json')
